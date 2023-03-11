ba0b53d404d0da25c4c69d6482d138ad2983e198
therealmarc123 <marc.goering@gmail.com>
2023-03-11 17:12:19 +0100

    Model for Movie and Genre done.

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Genre.java
@@ -0,0 +1,18 @@
+package at.ac.fhcampuswien.fhmdb.models;
+
+public enum Genre {
+    ALL,
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    CRIME,
+    DRAMA,
+    FANTASY,
+    HISTORICAL,
+    HORROR,
+    MYSTERY,
+    ROMANCE,
+    SCIENCE_FICTION,
+    THRILLER,
+    WESTERN
+}
