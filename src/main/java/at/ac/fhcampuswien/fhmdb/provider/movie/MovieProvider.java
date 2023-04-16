a8ffe56da5ffcb01aaaacc500efc3437318cacc3
drewitzmax <drewitz.max@googlemail.com>
2023-04-16 21:11:50 +0200

    fix Interface

--- src/main/java/at/ac/fhcampuswien/fhmdb/provider/movie/MovieProvider.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/provider/movie/MovieProvider.java
@@ -4,7 +4,10 @@ import at.ac.fhcampuswien.fhmdb.models.Movie;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public interface MovieProvider {
     List<Movie> getMovies();
+
+    List<Movie> getMoviesWithQuery(Map<String, String> queryMap);
 }
