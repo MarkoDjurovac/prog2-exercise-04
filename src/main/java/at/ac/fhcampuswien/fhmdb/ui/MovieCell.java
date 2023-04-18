20c2af1a6ff1dee3ad6b39dcb7301f96712346e7
therealmarc123 <marc.goering@gmail.com>
2023-04-18 21:51:37 +0200

    Added Rating and Release Year to MovieCell and updated MovieCell

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -2,6 +2,7 @@ package at.ac.fhcampuswien.fhmdb.ui;
 
 import at.ac.fhcampuswien.fhmdb.models.Movie;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.Background;
@@ -15,7 +16,9 @@ public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
     private final Label genres = new Label();
-    private final VBox layout = new VBox(title, detail, genres);
+    private final Label rating = new Label();
+    private final Label releaseYear = new Label();
+    private final VBox layout = new VBox(title, detail, genres, rating, releaseYear);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -33,12 +36,20 @@ public class MovieCell extends ListCell<Movie> {
                             : "No description available"
             );
 
-            genres.setText(String.join(", ", movie.getGenres().stream().map(String::toString).collect(Collectors.toList())));
+            genres.setText("Genres: " + String.join(", ", movie.getGenres().stream().map(String::toString).collect(Collectors.toList())));
+
+            String str = new StringBuilder().append(movie.getRating()).toString();
+            rating.setText("Rating: " + str);
+
+            String str2 = new StringBuilder().append(movie.getReleaseYear()).toString();
+            releaseYear.setText("Release Year: " + str2);
 
             // color scheme
             title.getStyleClass().add("text-yellow");
             detail.getStyleClass().add("text-white");
             genres.getStyleClass().addAll("text-white", "text-italic");
+            rating.getStyleClass().add("text-white");
+            releaseYear.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
@@ -48,6 +59,7 @@ public class MovieCell extends ListCell<Movie> {
             layout.setPadding(new Insets(10));
             layout.spacingProperty().set(10);
             layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
+            releaseYear.alignmentProperty().set(Pos.CENTER_RIGHT);
             setGraphic(layout);
         }
     }
