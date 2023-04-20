5fe87dbb794fa6845e590fc72b8ec8498f9cac76
therealmarc123 <marc.goering@gmail.com>
2023-04-20 20:26:30 +0200

    Updated Movie Class and added MovieTest.

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,24 +1,23 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
-    private String id;
-    private String title;
-    private List<String> genres;
-    private int releaseYear;
-    private String description;
-    private String imageUrl;
-    private int lengthInMinutes;
-    private List<String> directors;
-    private List<String> writers;
-    private List<String> mainCast;
-    private double rating;
+    private final String id;
+    private final String title;
+    private final List<String> genres;
+    private final int releaseYear;
+    private final String description;
+    private final String imgUrl;
+    private final int lengthInMinutes;
+    private final List<String> directors;
+    private final List<String> writers;
+    private final List<String> mainCast;
+    private final double rating;
 
-    public Movie( String id, String title, List<String> genres, int releaseYear, String description, String imageUrl,
-                  int lengthInMinutes, List<String> directors, List<String> writers, List<String> mainCast, double rating) {
+    public Movie(String id, String title, List<String> genres, int releaseYear, String description, String imgUrl,
+                 int lengthInMinutes, List<String> directors, List<String> writers, List<String> mainCast, double rating) {
 
         if(id == null || id.isBlank()){
             throw new IllegalArgumentException("ID may not be null or empty");
@@ -29,25 +28,28 @@ public class Movie {
         if(genres == null){
             throw new IllegalArgumentException("Genre-List may not be null");
         }
-        if(releaseYear < 1930 || releaseYear > 2023){
+        if(releaseYear < 1970 || releaseYear > 2023){
             throw new IllegalArgumentException("Invalid releaseYear");
         }
         if(description == null || description.isBlank()){
             throw new IllegalArgumentException("Description may not be null or empty");
         }
-        if(imageUrl == null || imageUrl.isBlank()){
+        if(imgUrl == null || imgUrl.isBlank()){
             throw new IllegalArgumentException("Image URL may not be null or empty");
         }
-        if( directors == null){
+        if(lengthInMinutes < 0){
+            throw new IllegalArgumentException("Invalid length in minutes");
+        }
+        if(directors == null){
             throw new IllegalArgumentException("Directors-List may not be null");
         }
-        if( writers == null){
+        if(writers == null){
             throw new IllegalArgumentException("Writers-List may not be null");
         }
-        if( mainCast == null){
+        if(mainCast == null){
             throw new IllegalArgumentException("MainCast-List may not be null");
         }
-        if(rating < 0 || rating > 10){
+        if(rating < 0.0 || rating > 10.0){
             throw new IllegalArgumentException("Invalid rating");
         }
 
@@ -56,7 +58,7 @@ public class Movie {
         this.genres = new ArrayList<>(genres);
         this.releaseYear = releaseYear;
         this.description = description;
-        this.imageUrl = imageUrl;
+        this.imgUrl = imgUrl;
         this.lengthInMinutes = lengthInMinutes;
         this.directors = new ArrayList<>(directors);
         this.writers = new ArrayList<>(writers);
@@ -92,8 +94,8 @@ public class Movie {
         return id;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
     public int getLengthInMinutes() {
@@ -108,15 +110,4 @@ public class Movie {
         return releaseYear;
     }
 
-    public boolean hasGenre( String genre ) {
-        return genres.contains(genre);
-    }
-
-    public boolean hasReleaseYear( int releaseYear ) {
-        return this.releaseYear == releaseYear;
-    }
-
-    public boolean hasRating( double rating ) {
-        return this.rating == rating;
-    }
 }
