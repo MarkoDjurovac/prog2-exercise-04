d048d6dd38e69aa22f0f7866d236fd9e6e2f977e
therealmarc123 <marc.goering@gmail.com>
2023-04-20 20:12:41 +0200

    Updated Movie Class

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,22 +1,23 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
-    private final String id;
-    private final String title;
-    private final List<String> genres;
-    private final int releaseYear;
-    private final String description;
-    private final String imageUrl;
-    private final int lengthInMinutes;
-    private final List<String> directors;
-    private final List<String> writers;
-    private final List<String> mainCast;
-    private final double rating;
+    private String id;
+    private String title;
+    private List<String> genres;
+    private int releaseYear;
+    private String description;
+    private String imageUrl;
+    private int lengthInMinutes;
+    private List<String> directors;
+    private List<String> writers;
+    private List<String> mainCast;
+    private double rating;
 
-    public Movie(String id, String title, List<String> genres, int releaseYear, String description, String imageUrl,
+    public Movie( String id, String title, List<String> genres, int releaseYear, String description, String imageUrl,
                   int lengthInMinutes, List<String> directors, List<String> writers, List<String> mainCast, double rating) {
 
         if(id == null || id.isBlank()){
@@ -28,7 +29,7 @@ public class Movie {
         if(genres == null){
             throw new IllegalArgumentException("Genre-List may not be null");
         }
-        if(releaseYear < 1970 || releaseYear > 2023){
+        if(releaseYear < 1930 || releaseYear > 2023){
             throw new IllegalArgumentException("Invalid releaseYear");
         }
         if(description == null || description.isBlank()){
@@ -37,19 +38,16 @@ public class Movie {
         if(imageUrl == null || imageUrl.isBlank()){
             throw new IllegalArgumentException("Image URL may not be null or empty");
         }
-        if(lengthInMinutes < 0){
-            throw new IllegalArgumentException("Invalid length in minutes");
-        }
-        if(directors == null){
+        if( directors == null){
             throw new IllegalArgumentException("Directors-List may not be null");
         }
-        if(writers == null){
+        if( writers == null){
             throw new IllegalArgumentException("Writers-List may not be null");
         }
-        if(mainCast == null){
+        if( mainCast == null){
             throw new IllegalArgumentException("MainCast-List may not be null");
         }
-        if(rating < 0.0 || rating > 10.0){
+        if(rating < 0 || rating > 10){
             throw new IllegalArgumentException("Invalid rating");
         }
 
@@ -108,5 +106,17 @@ public class Movie {
 
     public int getReleaseYear() {
         return releaseYear;
+    }
+
+    public boolean hasGenre( String genre ) {
+        return genres.contains(genre);
+    }
+
+    public boolean hasReleaseYear( int releaseYear ) {
+        return this.releaseYear == releaseYear;
+    }
+
+    public boolean hasRating( double rating ) {
+        return this.rating == rating;
     }
 }
