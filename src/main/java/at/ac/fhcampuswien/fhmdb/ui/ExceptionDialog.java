e634dfabc97f6f0c5aeecdf333fff27d7a109d9d
Marko Djurovac <marko.djurovac@stud.fh-campuswien.ac.at>
2023-05-15 18:05:47 +0200

    Improved error messaging in ExceptionDialog.

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/ExceptionDialog.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/ExceptionDialog.java
@@ -1,6 +1,8 @@
 package at.ac.fhcampuswien.fhmdb.ui;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 
 public class ExceptionDialog
 {
@@ -8,7 +10,16 @@ public class ExceptionDialog
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("An error occurred!");
         alert.setHeaderText(e.getMessage());
-        alert.setContentText("Error caused by:\n\n" + e.getCause());
+        String contentText = "Error caused by:\n" + e.getCause();
+        ScrollPane scrollPane = new ScrollPane();
+        TextArea textArea = new TextArea(contentText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+        scrollPane.setContent(textArea);
+        scrollPane.setPrefSize(400, 100);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        alert.getDialogPane().setContent(scrollPane);
         alert.showAndWait();
     }
 }
